389b5a190bedf8acf76859a35dc041a8536c99ed
qk27015 <tk5760a@gmail.com>
2018-12-16 20:50:19 +0900

    1.5

--- 01/01/01.cpp
+++ 01/01/01.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 using namespace std;
 
-// 1.1 ある文字列が、全て固有であるかどうかを判定するアルゴリズムを実装
+// 1.1 : ある文字列が、全て固有であるかどうかを判定するアルゴリズムを実装
 // 1 / 189
 bool isUniqueStr(string str)
 {
@@ -27,7 +27,7 @@ bool isUniqueStr(string str)
 
 
 
-// 1.2 2つの文字列が、片方が片方の並べ替えになっているかを判定
+// 1.2 : 2つの文字列が、片方が片方の並べ替えになっているかを判定
 // 2 / 189
 bool isPermutation(string s1, string s2)
 {
@@ -45,6 +45,113 @@ bool isPermutation(string s1, string s2)
 
 
 
+// 1.3 : すべての空白文字を%20で置き換える
+// 3 / 189
+string encode(string str)
+{
+	vector<char> v;
+	
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] == ' ')
+		{
+			v.push_back('%'); v.push_back('2'); v.push_back('0');
+		}
+		else
+			v.push_back(str[i]);
+	}
+	
+	string result(v.begin(), v.end());
+	return result;
+}
+
+
+
+// 1.4 : 文字列が回文の順列(回文を作成できる)であるかを調べる
+// 4 / 189
+bool isPalindrome(string str)
+{
+	const int kMaxChar = 255;
+	int numChar[kMaxChar];
+	for (int i = 0; i < kMaxChar; i++) numChar[i] = 0;
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		numChar[str[i]]++;
+	}
+
+	int numOdd = 0, numEven = 0;
+	for (int i = 0; i < kMaxChar; i++)
+	{
+		if (numChar[i] % 2 == 1 && numChar[i] != 0)
+			numOdd++;
+		if (numChar[i] % 2 == 0 && numChar[i] != 0)
+			numEven++;
+	}
+
+	if (numOdd == 1)
+		return true;
+	if (numOdd == 0 && numEven > 0)
+		return true;
+	return false;
+}
+
+
+
+// 1.5 : 文字列1に対して、文字の挿入・削除・置換を一度だけ行い、文字列2を作成できるか調べる
+// 5 / 189
+bool canReplace(string s1, string s2);
+bool canDelete(string s1, string s2);
+
+bool canCreate(string s1, string s2)
+{
+	if (s1.length() == s2.length())
+		return canReplace(s1, s2);
+	if (s1.length() - 1 == s2.length())
+		return canDelete(s1, s2);
+	if (s1.length() + 1 == s2.length())
+		return canDelete(s2, s1);
+	return false;
+}
+
+bool canReplace(string s1, string s2)
+{
+	int cntDiffChar = 0;
+	for (int i = 0; i < s1.length(); i++)
+	{
+		if (s1[i] != s2[i])
+			cntDiffChar++;
+	}
+	return cntDiffChar < 2;
+}
+
+bool canDelete(string s1, string s2)
+{
+	bool *canFlagArray = (bool *)malloc(sizeof(bool) * s2.length());
+	for (int i = 0; i < s2.length(); i++)
+		canFlagArray[i] = false;
+
+	for (int i = 0; i < s2.length(); i++)
+	{
+		for (int j = 0; j < s1.length(); j++)
+		{
+			if (s1[j] == s2[i])
+			{
+				canFlagArray[i] = true;
+				break;
+			}
+		}
+	}
+
+	for (int i = 0; i < s2.length(); i++)
+	{
+		if (canFlagArray[i] == false) return false;
+	}
+	return true;
+}
+
+
+
 int main()
 {
 	// 1.1
@@ -56,6 +163,23 @@ int main()
 	cout << isPermutation("abcdef", "fedacb") << endl;
 	cout << isPermutation("abcccca", "abxxxxa") << endl;
 	cout << endl;
+
+	// 1.3
+	cout << encode("a bc") << endl;
+	cout << endl;
+
+	// 1.4
+	cout << isPalindrome("tactcoapapa") << endl;
+	cout << isPalindrome("abba") << endl;
+	cout << isPalindrome("acbxyz") << endl;
+	cout << isPalindrome("ab") << endl;
+	cout << endl;
+
+	// 1.5
+	cout << canCreate("ple", "pale") << endl;
+	cout << canCreate("pale", "ple") << endl;
+	cout << canCreate("pale", "bale") << endl;
+	cout << canCreate("pale", "bake") << endl;
 
 	return 0;
 }
